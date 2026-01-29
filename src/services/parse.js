e05bedcdb1623e71f1be84c2a40f3a6917be87fd
Jocel <jocel.junior.silva@gmail.com>
2026-01-28 23:38:34 -0300

    Fix: Use correct JavaScript Key for Kas Ballet app

--- src/services/parse.js
+++ src/services/parse.js
@@ -1,9 +1,9 @@
 import Parse from 'parse'
 
-// Configuração do Back4App
+// Configuração do Back4App - Kas Ballet
 Parse.initialize(
   'x0glFszYc9puGSYr1ODyza7Qej1PkGiEwM7VAj6Y', // Application ID
-  'ZumqURJep4l4ui2tAkcoZ3aTEJHJJbJXf4j8U3DW'  // JavaScript Key
+  'LOFYaSLCjZ3ui1giaPzHB4TTACWz0i14yIvCT1jC'  // JavaScript Key
 )
 
 Parse.serverURL = 'https://parseapi.back4app.com/'
